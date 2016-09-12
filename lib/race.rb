a44077ac59bba8c45bf54bc1dbd3bab8f5f399ec
Veselin Stoyanov <veselin1996@gmail.com>
2016-09-12 15:23:42 +0300

    Begin with some basic interface.

--- lib/race.rb
+++ lib/race.rb
@@ -1,8 +1,13 @@
 module RaceBet
   class Race
 
+    def initialize(guesses, winners)
+      @guesses, @winners = guesses, winners
+    end
+
     class << self
       def score(guesses, winners)
+
 
       end
     end
