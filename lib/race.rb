e8bea15fd4b338016744dd86bba56d3f0905b8d9
Veselin Stoyanov <veselin1996@gmail.com>
2016-09-12 15:54:58 +0300

    Implemented score calculation logic.

--- lib/race.rb
+++ lib/race.rb
@@ -1,25 +1,28 @@
 module RaceBet
   class Race
 
+    attr_accessor :score
+
     def initialize(guesses, winners)
       @guesses, @winners = guesses, winners
-    end
-
-    def calculate_score
-      # TODO
+      @score = 0
+      calculate_score
     end
 
     private
 
-    AWARDED_POINTS = [15, 10, 5, 3, 1]
+    AWARDS = [15, 10, 5, 3, 1]
 
-    def calculate_exact_guesses
-
+    def calculate_score
+      @guesses.each_with_index do |value, index|
+        self.score += AWARDS[index] and next if value == @winners[index]
+        self.score += 1 if @winners.include?(value)
+      end
     end
 
     class << self
       def score(guesses, winners)
-        RaceBet::Race.new(guesses, winners).calculate_score
+        RaceBet::Race.new(guesses, winners).score
       end
     end
 
