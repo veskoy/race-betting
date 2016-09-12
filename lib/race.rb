4f52acbe2e5bea977e1bcebd949f0e591d3b5234
Veselin Stoyanov <veselin1996@gmail.com>
2016-09-12 15:31:15 +0300

    Design how calculation will be handled.

--- lib/race.rb
+++ lib/race.rb
@@ -5,10 +5,21 @@ module RaceBet
       @guesses, @winners = guesses, winners
     end
 
+    def calculate_score
+      # TODO
+    end
+
+    private
+
+    AWARDED_POINTS = [15, 10, 5, 3, 1]
+
+    def calculate_exact_guesses
+
+    end
+
     class << self
       def score(guesses, winners)
-
-
+        RaceBet::Race.new(guesses, winners).calculate_score
       end
     end
 
